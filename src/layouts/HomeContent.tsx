9c258decaa4e135879c921bdfa75306d781f780c
yassineelhoubi <elhoubiyassine@gmail.com>
2022-05-01 16:55:10 +0000

    Customize home page components

--- src/layouts/HomeContent.tsx
+++ src/layouts/HomeContent.tsx
@@ -3,70 +3,70 @@ import { Link } from 'react-router-dom';
 
 
 const HomeContent: React.FC = () => {
-    return (
-          <div className="h-full w-full px-6 grid grid-cols-3 lg:grid-cols-3 gap-2">
-            <Link
-              to="/withdraw"
-              className="relative border border-gray-100 bg-white rounded-tl-3xl w-full h-full flex flex-col justify-center items-center"
-            >
-              <img src="src/assets/euro.gif" alt="" className="w-1/4" />
-              <h2 className="font-semibold text-sm lg:text-2xl">
-                Faire un retrait
-              </h2>
-            </Link>
+  return (
+    <div className="h-full w-full px-6 grid grid-cols-3 lg:grid-cols-3 gap-2">
+      <Link
+        to="/withdraw"
+        className="relative border border-gray-100 bg-white rounded-tl-3xl w-full h-full flex flex-col justify-center items-center"
+      >
+        <img src="src/assets/euro.gif" alt="" className="w-1/4" />
+        <h2 className="font-semibold text-sm lg:text-2xl">
+          Withdrawal
+        </h2>
+      </Link>
 
-            <Link
-              to="/bills"
-              className="relative flex flex-col justify-center items-center border border-gray-100 bg-white w-full h-full"
-            >
-              <img src="src/assets/smartphone.gif" alt="" className="w-1/4" />
-              <h2 className="font-semibold text-base lg:text-2xl">
-                Payer Facture{' '}
-              </h2>
-            </Link>
+      <Link
+        to="/bills"
+        className="relative flex flex-col justify-center items-center border border-gray-100 bg-white w-full h-full"
+      >
+        <img src="src/assets/smartphone.gif" alt="" className="w-1/4" />
+        <h2 className="font-semibold text-base lg:text-2xl">
+          Pay bills
+        </h2>
+      </Link>
 
-            <Link
-              to="/cash"
-              className="relative flex flex-col justify-center items-center border border-gray-100 bg-white rounded-tr-3xl w-full h-full"
-            >
-              <img src="src/assets/box.gif" alt="" className="w-1/4" />
-              <h2 className="font-semibold text-sm lg:text-2xl">
-                Consulter Solde
-              </h2>
-            </Link>
+      <Link
+        to="/cash"
+        className="relative flex flex-col justify-center items-center border border-gray-100 bg-white rounded-tr-3xl w-full h-full"
+      >
+        <img src="src/assets/box.gif" alt="" className="w-1/4" />
+        <h2 className="font-semibold text-sm lg:text-2xl">
+          Consult Balance
+        </h2>
+      </Link>
 
-            <Link
-              to="/car"
-              className="relative flex flex-col justify-center items-center border border-gray-100 bg-white rounded-bl-3xl w-full h-4/5"
-            >
-              <img src="src/assets/car.gif" alt="" className="w-1/4" />
-              <h2 className="font-semibold text-sm lg:text-2xl">
-                Vignette de voiture
-              </h2>
-            </Link>
+      <Link
+        to="/car"
+        className="relative flex flex-col justify-center items-center border border-gray-100 bg-white rounded-bl-3xl w-full h-4/5"
+      >
+        <img src="src/assets/car.gif" alt="" className="w-1/4" />
+        <h2 className="font-semibold text-sm lg:text-2xl">
+          Car Vignette
+        </h2>
+      </Link>
 
-            <Link
-              to="/booking"
-              className="relative flex flex-col justify-center items-center border border-gray-100 bg-white w-full h-4/5"
-            >
-              <img src="src/assets/airplane.gif" alt="" className="w-1/4" />
-              <h2 className="font-semibold text-sm lg:text-2xl">
-                Ticket du Voyage
-              </h2>
-            </Link>
+      <Link
+        to="/booking"
+        className="relative flex flex-col justify-center items-center border border-gray-100 bg-white w-full h-4/5"
+      >
+        <img src="src/assets/airplane.gif" alt="" className="w-1/4" />
+        <h2 className="font-semibold text-sm lg:text-2xl">
+          Travel Ticket
+        </h2>
+      </Link>
 
-            <Link
-              to="/mobile"
-              className="relative flex flex-col justify-center items-center border border-gray-100 bg-white rounded-br-3xl w-full h-4/5"
-            >
-              <img src="src/assets/mobile.gif" alt="" className="w-1/4" />
-              <h2 className="font-semibold text-sm lg:text-2xl">
-                Rercharge mobile
-              </h2>
-            </Link>
-          </div>
-     
-    );
+      <Link
+        to="/mobile"
+        className="relative flex flex-col justify-center items-center border border-gray-100 bg-white rounded-br-3xl w-full h-4/5"
+      >
+        <img src="src/assets/mobile.gif" alt="" className="w-1/4" />
+        <h2 className="font-semibold text-sm lg:text-2xl">
+          Mobile Recharge
+        </h2>
+      </Link>
+    </div>
+
+  );
 };
 
 export default HomeContent;
--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 
 const Navbar: React.FC = () => {
-    return (
-      <div className="h-24 w-full flex justify-between items-center pr-16">
-        <div className="text-white font-bold">
-          <img src="src/assets/t.svg" alt="" className="w-72 mb-8" />
-        </div>
-        <div className="bg-black/20 w-1/5 rounded-full flex">
-          <p className="p-2 w-1/2 text-center">EN</p>
-          <p className="bg-white p-2 w-1/2 rounded-full text-center">FR</p>
-        </div>
+  return (
+    <div className="h-24 w-full flex justify-between items-center pr-16">
+      <div className="text-white font-bold">
+        <img src="src/assets/t.svg" alt="" className="w-72 mb-8" />
       </div>
-    );
+      <div className="bg-black/20 w-1/5 rounded-full flex">
+        <p className="p-2 w-1/2 text-center">FR</p>
+        <p className="bg-white p-2 w-1/2 rounded-full text-center">EN</p>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
