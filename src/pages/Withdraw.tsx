c59a967910ca4f560ba3d915fc8ff78bd80696cb
yassineelhoubi <elhoubiyassine@gmail.com>
2022-05-01 15:42:30 +0000

    Customize Withdraw page

--- src/pages/Withdraw.tsx
+++ src/pages/Withdraw.tsx
@@ -3,11 +3,26 @@ import '../assets/css/custom.css';
 import { GiReturnArrow } from 'react-icons/gi';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import Snackbar from '@mui/material/Snackbar';
+import Slide, { SlideProps } from '@mui/material/Slide';
+import { useNavigate } from 'react-router-dom';
 
+// snackbar transition
+type TransitionProps = Omit<SlideProps, 'direction'>;
 
+function TransitionUp(props: TransitionProps) {
+  return <Slide {...props} direction="up" />;
+}
 
 const Withdraw: React.FC = () => {
-  const action = (balance: number) => {
+
+  const navigate = useNavigate();
+  
+  const [transition, setTransition] = React.useState<
+    React.ComponentType<TransitionProps> | undefined>(undefined);
+  const [open, setOpen] = React.useState(false);
+
+  const action = (balance: number, Transition: React.ComponentType<TransitionProps>) => {
     const userId = JSON.parse(localStorage.getItem('user') as string)._id;
     const data = {
       balance: balance
@@ -15,6 +30,11 @@ const Withdraw: React.FC = () => {
     axios.patch(`http://localhost:3030/user/${userId}/withdraw-balance`, data)
       .then(res => {
         console.log(res);
+        setTransition(() => Transition);
+        setOpen(true);
+        setTimeout(() => {
+          navigate('/home');
+        }, 2000);
       })
       .catch(err => {
         console.log(err);
@@ -23,6 +43,14 @@ const Withdraw: React.FC = () => {
 
   return (
     <>
+      <Snackbar
+        open={open}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        TransitionComponent={transition}
+        autoHideDuration={6000}
+        message="Take Your Card"
+
+      />
       <div className="w-full flex justify-between">
         <span></span>
         <h1 className="text-3xl lg:text-5xl uppercase bg-clip-text text-transparent bg-gradient-to-r from-cyan-300 to-violet-200 font-extrabold">
@@ -34,29 +62,29 @@ const Withdraw: React.FC = () => {
       </div>
       <div className="w-full h-5/6 flex">
         <div className="w-1/2 h-full rounded-3xl flex flex-col justify-around items-center">
-          <button className="w-4/5 btn h-1/6" onClick={() => action(100)}>
+          <button className="w-4/5 btn h-1/6" onClick={() => action(100, TransitionUp)}>
             <span className="text-cyan-900 text-3xl lg:text-5xl font-extrabold">
               100DH
             </span>
           </button>
-          <button className="w-4/5 btn h-1/6 rounded-r-full" onClick={() => action(200)}>
+          <button className="w-4/5 btn h-1/6 rounded-r-full" onClick={() => action(200, TransitionUp)}>
             <span className="text-cyan-900 text-3xl lg:text-5xl font-extrabold">
               200DH
             </span>
           </button>
-          <button className="w-4/5 btn h-1/6 rounded-r-full" onClick={() => action(300)}>
+          <button className="w-4/5 btn h-1/6 rounded-r-full" onClick={() => action(300, TransitionUp)}>
             <span className="text-cyan-900 text-3xl lg:text-5xl font-extrabold">
               300DH
             </span>
           </button>
         </div>
         <div className="w-1/2 h-full rounded-3xl flex flex-col items-center justify-around">
-          <button className="w-4/5 btn h-1/6 rounded-l-full" onClick={() => action(400)}>
+          <button className="w-4/5 btn h-1/6 rounded-l-full" onClick={() => action(400, TransitionUp)}>
             <span className="text-cyan-900 text-3xl lg:text-5xl font-extrabold">
               400DH
             </span>
           </button>
-          <button className="w-4/5 btn h-1/6 rounded-l-full" onClick={() => action(500)}>
+          <button className="w-4/5 btn h-1/6 rounded-l-full" onClick={() => action(500, TransitionUp)}>
             <span className="text-cyan-900 text-3xl lg:text-5xl font-extrabold">
               500DH
             </span>
